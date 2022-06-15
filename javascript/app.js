68415e5f7e27d1920f3befd5c0ee286a837aa8f0
Daniel <danielpsc1993@gmail.com>
2022-06-15 17:31:48 -0300

    versionamento responsivo

--- javascript/app.js
+++ javascript/app.js
@@ -3,11 +3,19 @@ let home = document.getElementById('navHome');
 let menu = document.getElementById('navMenu');
 let delivery = document.getElementById('navDelivery');
 let btn =  document.getElementById('btn-responsive');
-let teste = document.getElementById('menuResponsivo')
+let teste = document.getElementById('menuResponsivo');
+let bannerLeft1 = document.getElementById('bannerLeftTopo1');
+let bannerLeft2 = document.getElementById('bannerLeftTopo2');
+let bannerLeft3 = document.getElementById('bannerLeftTopo3');
+let body = document.getElementById('indexHtml');
 
 btn.addEventListener('click',()=>{
     btn.classList.toggle('active')
     teste.classList.toggle('active')
+    bannerLeft1.classList.toggle('active');
+    bannerLeft2.classList.toggle('active');
+    bannerLeft3.classList.toggle('active');
+    body.classList.toggle('active');
 })
 
 
